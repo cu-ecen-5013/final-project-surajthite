6c55d743f977f521ae529662abc124cbcaa7efbf
surajthite <suraj.thite@colorado.edu>
2020-04-22 12:08:45 -0700

    Rootfs overlay added for extra credit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 997ffc261fa959ddfe7c4a7f9647a0f70d4e23d0
+AESD_ASSIGNMENTS_VERSION = 6850648011cd582de8b9657539dffe0d9117a2ac
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/s20-remote-health-monitoring.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -19,6 +19,7 @@ endef
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/tmp102/tmp102 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/tmp102/post_test $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/tmp102_start_stop.sh $(TARGET_DIR)/etc/init.d/S99tmp102
 endef
 
