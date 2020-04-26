33d56b98bfd36495f2b2ddda4197d68ee697161f
surajthite <suraj.thite@colorado.edu>
2020-04-26 13:40:03 -0700

    Ping Functionality Added

--- base_external/rootfs_overlay/bin/mqtt/publisher.py
+++ base_external/rootfs_overlay/bin/mqtt/publisher.py
@@ -8,54 +8,41 @@ import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.client as mqtt
 import sys
 import urllib2
-
+import socket
+import os
 from datetime import datetime
+
 print("**************Sending temperature to MQTT Server*****************")
-#print 'Argument List:', str(sys.argv)
 client = mqtt.Client()
-client.connect("10.0.0.20",1883,60)
-#x=raw_input();
-print(sys.argv[1])
-#d_t = str(datetime.now())
-#print(d_t)
-
 now = datetime.now() 
-#print("now =", now)
-
 dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
 print("date and time = " + dt_string)	
-
 curr_date = now.strftime("%m/%d/%Y")
 curr_time = now.strftime("%H:%M:%S")
-
 temp_value = sys.argv[1].split(":")[1]
 temp_value = temp_value[:5] + " C"
 
+data_to_send = "T1                 |        " + curr_date + "--" + curr_time + "        | " + "       " + temp_value + "     |"
 
-# data_to_send = dt_string + str(sys.argv[1])
-
-#data_to_send = dt_string + " " + temp_value
-
-data_to_send = "| " + curr_date + " | " + curr_time + " | " + " " + temp_value + " |"
-
-#data_to_send_2 = "____________________________________"
-
+server_ip = '10.0.0.20 -s 1 -c 1 -q'
 def internet_on():
-    try:
-        urllib2.urlopen('http://10.0.0.20', timeout=1)
-        print("**************Connected to server***********************")
-	client.publish("topic/test",data_to_send)
-    except urllib2.URLError as err: 
-        print("**************Disconnected from Server******************")
-	file_handler = open('/usr/tmp102_backup.txt', 'a')
-	file_handler.write(data_to_send)
-  	file_handler.write("\n")
-  	file_handler.close()
+  
+	print("**************Pinging to Server******************")
+	rep = os.system('ping ' + server_ip)
+	if rep == 0:
+ 	 print("**************Ping Successful to server***********************")
+ 	 client.connect("10.0.0.20",1883,60) 
+	 client.publish("topic/test",data_to_send)
+	else:
+	 print("**************Writing to BackUp file******************")
+	 file_handler = open('/usr/tmp102_backup.txt', 'a')
+	 file_handler.write(data_to_send)
+  	 file_handler.write("\n")
+  	 file_handler.close()
 
 internet_on()
 #client.publish("topic/test",data_to_send)
 #client.publish("topic/test",data_to_send_2)
 #client.publish("topic/test",str(sys.argv[1]))
-print(sys.argv[1])
+print('Current Data Point | ' + sys.argv[1])
 client.disconnect();
-
